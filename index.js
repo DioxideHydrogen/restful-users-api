00edc49f0a43e5f6e4b0eb82104e5d83d77f7eea
Hugo Henrique <hugoaguiar70@hotmail.com>
2024-09-10 18:36:18 -0300

    fix|refact: fix delete all users route and removed server listen from index

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.get('/users/removeds', userController.removeds);
 app.post('/users', userController.store);
 app.get('/users/:id', userController.show);
 app.put('/users/:id', userController.update);
-app.delete('/users/:id', userController.destroy);
 app.delete('/users/all', userController.destroyAll);
-app.listen(process.env.APP_PORT || 3000, () => {
-	console.log('Server is running in port:', process.env.APP_PORT || 3000);
-});
+app.delete('/users/:id', userController.destroy);
+
+module.exports = app;
